0756c31d2bf2f4cf7dd0ad7e3176e9feaefe3197
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2022-09-16 16:51:56 +0200

    ADD: PUT and DELETE method to '/api/minions/:minionId' path

--- server/minions.js
+++ server/minions.js
@@ -5,8 +5,8 @@
 GET /api/minions to get an array of all minions. - OK
 POST /api/minions to create a new minion and save it to the database. OK
 GET /api/minions/:minionId to get a single minion by id. - OK
-PUT /api/minions/:minionId to update a single minion by id.
-DELETE /api/minions/:minionId to delete a single minion by id.*/
+PUT /api/minions/:minionId to update a single minion by id. - OK
+DELETE /api/minions/:minionId to delete a single minion by id.- OK */
 
 const minionsRouter = require('express').Router();
 
@@ -14,8 +14,10 @@ module.exports = minionsRouter;
 
 const { 
     addToDatabase,
+    deleteFromDatabasebyId,
     getAllFromDatabase,
-    getFromDatabaseById
+    getFromDatabaseById,
+    updateInstanceInDatabase
 } = require('./db');
 
 minionsRouter.param('minionId', (req, res, next, id) => {
@@ -40,4 +42,19 @@ minionsRouter.post('/', (req, res) => {
 
 minionsRouter.get('/:minionId', (req, res) => {
     res.send(req.minion);
+});
+
+minionsRouter.put('/:minionId', (req, res) => {
+    let updatedMinion = updateInstanceInDatabase('minions', req.body);
+    res.status(201).send(updatedMinion);
+});
+
+minionsRouter.delete('/:minionId', (req, res) => {
+    const deletedMinion = deleteFromDatabasebyId('minions', req.params.minionId);
+    if(deletedMinion){
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
 });
