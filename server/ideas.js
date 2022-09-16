841490ea14c14d4ef1107777891c385f0becafce
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2022-09-16 17:49:20 +0200

    ADD: GET, PUT and DELETE method to '/api/meetings' path

--- server/ideas.js
+++ server/ideas.js
@@ -1,3 +1,60 @@
+/*Required
+
+/api/ideas
+
+GET /api/ideas to get an array of all ideas. OK
+POST /api/ideas to create a new idea and save it to the database. OK
+GET /api/ideas/:ideaId to get a single idea by id. OK
+PUT /api/ideas/:ideaId to update a single idea by id. OK
+DELETE /api/ideas/:ideaId to delete a single idea by id. OK*/
+
 const ideasRouter = require('express').Router();
 
 module.exports = ideasRouter;
+
+const { 
+    addToDatabase,
+    deleteFromDatabasebyId,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase
+} = require('./db');
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } 
+  else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.get('/', (req, res) => {
+    res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.get('/:ideaId', (req, res) => {
+    res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res) => {
+    let updatedIdea = updateInstanceInDatabase('ideas', req.body);
+    res.status(201).send(updatedIdea);
+});
+
+ideasRouter.delete('/:ideaId', (req, res) => {
+    const deletedIdea= deleteFromDatabasebyId('ideas', req.params.ideaId);
+    if(deletedIdea){
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
+});
--- server/meetings.js
+++ server/meetings.js
@@ -1,3 +1,33 @@
+/*Required
+
+/api/meetings
+
+GET /api/meetings to get an array of all meetings. OK
+POST /api/meetings to create a new meeting and save it to the database. OK
+DELETE /api/meetings to delete all meetings from the database. OK */
+
 const meetingsRouter = require('express').Router();
 
 module.exports = meetingsRouter;
+
+const { 
+    addToDatabase,
+    createMeeting,
+    deleteAllFromDatabase,
+    getAllFromDatabase,
+} = require('./db');
+
+
+meetingsRouter.get('/', (req, res) => {
+    res.send(getAllFromDatabase('meetings'));
+});
+
+meetingsRouter.post('/', (req, res) => {
+    let newMeeting = addToDatabase('meetings', createMeeting());
+    res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res) => {
+    deleteAllFromDatabase('meetings');
+  res.status(204).send();
+});
